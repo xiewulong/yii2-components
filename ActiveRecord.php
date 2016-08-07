852b0e7c66daf7338a8e4ccd417bc56b8bb508cf
xiewulong <xiewulong@vip.qq.com>
2016-08-07 10:33:30 +0800

    components

---
+++ ActiveRecord.php
@@ -0,0 +1,46 @@
+<?php
+/*!
+ * yii2 extension - active record - component
+ * xiewulong <xiewulong@vip.qq.com>
+ * https://github.com/xiewulong/yii2-components
+ * https://raw.githubusercontent.com/xiewulong/yii2-components/master/LICENSE
+ * create: 2016/8/7
+ * update: 2016/8/7
+ * version: 0.0.1
+ */
+
+namespace yii\components;
+
+use Yii;
+
+class ActiveRecord extends \yii\db\ActiveRecord {
+
+	private $_firstErrorAttribute = false;
+
+	/**
+	 * Returns the first error's attribute name
+	 *
+	 * @since 0.0.1
+	 * @param {string} $attribute
+	 * @return {string|null}
+	 */
+	public function isFirstErrorAttribute($attribute) {
+		if($this->_firstErrorAttribute === false) {
+			$errorAttributes = array_keys($this->firstErrors);
+			$this->_firstErrorAttribute = array_shift($errorAttributes);
+		}
+
+		return $attribute == $this->_firstErrorAttribute;
+	}
+
+	/**
+	 * Returns the first error
+	 *
+	 * @since 0.0.1
+	 * @return {string|null}
+	 */
+	public function getFirstErrorInFirstErrors() {
+		return array_shift($this->firstErrors);
+	}
+
+}
---
+++ Controller.php
@@ -0,0 +1,49 @@
+<?php
+/*!
+ * yii2 extension - controller - component
+ * xiewulong <xiewulong@vip.qq.com>
+ * https://github.com/xiewulong/yii2-components
+ * https://raw.githubusercontent.com/xiewulong/yii2-components/master/LICENSE
+ * create: 2016/8/7
+ * update: 2016/8/7
+ * version: 0.0.1
+ */
+
+namespace yii\components;
+
+use Yii;
+
+class Controller extends \yii\web\Controller {
+
+	public function init() {
+		parent::init();
+
+		// set referrer from request
+		if($referrer = \Yii::$app->request->get('referrer')) {
+			\Yii::$app->user->setReturnUrl($referrer);
+		}
+	}
+
+	/**
+	 * ajax response
+	 *
+	 * @since 0.0.1
+	 * @param {string|array} [$response] response data
+	 * @param {string} [$format=json] response data format
+	 * @param {string} [$callbackParam] jsonp callback param name catched from request
+	 * @return {string|array|null}
+	 */
+	public function respond($response = null, $format = \yii\web\Response::FORMAT_JSON, $callbackParam = null) {
+		\Yii::$app->response->format = \Yii::$app->request->get('format', $format);
+		if(($callback = \Yii::$app->request->get($callbackParam)) && \Yii::$app->response->format == 'jsonp' && $response) {
+			$_response = $response;
+			$response = [
+				'data' => $_response,
+				'callback' => $callback,
+			];
+		}
+
+		return $response;
+	}
+
+}
