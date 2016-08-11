f87e895509a4cc3306751d8f2363742914270944
xiewulong <xiewulong@vip.qq.com>
2016-08-12 01:51:19 +0800

    components

--- ActiveRecord.php
+++ ActiveRecord.php
@@ -5,17 +5,74 @@
  * https://github.com/xiewulong/yii2-components
  * https://raw.githubusercontent.com/xiewulong/yii2-components/master/LICENSE
  * create: 2016/8/7
- * update: 2016/8/7
+ * update: 2016/8/11
  * since: 0.0.1
  */
 
 namespace yii\components;
 
 use Yii;
+use yii\validators\FileValidator;
+use yii\web\UploadedFile;
 
 class ActiveRecord extends \yii\db\ActiveRecord {
 
+	// private $_activeFileValidator;
+
 	private $_firstErrorAttribute = false;
+
+	/**
+	 * @inheritdoc
+	 */
+	// public function beforeValidate() {
+	// 	return parent::beforeValidate() && $this->setUploadedFiles();
+	// }
+
+	/**
+	 * Check active FileValidator and set UploadedFile object to attributes
+	 *
+	 * @since 0.0.1
+	 * @return {boolean}
+	 */
+	// private function setUploadedFiles() {
+	// 	$scenario = $this->getScenario();
+	// 	foreach($this->getValidators() as $validator) {
+	// 		if($validator instanceof FileValidator && $validator->isActive($scenario)) {
+	// 			$this->_activeFileValidator = $validator;
+	// 		}
+	// 	}
+
+	// 	if($this->_activeFileValidator) {
+	// 		foreach($this->_activeFileValidator->attributes as $attribute) {
+	// 			$this->$attribute = $this->_activeFileValidator->maxFiles === 1 ? UploadedFile::getInstance($this, $attribute) : UploadedFile::getInstances($this, $attribute);
+	// 		}
+	// 	}
+
+	// 	return true;
+	// }
+
+	/**
+	 * Save uploaded files
+	 *
+	 * @since 0.0.1
+	 * @return {boolean}
+	 */
+	// public function saveUploadedFiles() {
+	// 	if($this->_activeFileValidator) {
+	// 		foreach($this->_activeFileValidator->attributes as $attribute) {
+	// 			if($this->_activeFileValidator->maxFiles === 1) {
+	// 				$file = $this->$attribute;
+	// 				$file->saveAs('assets/' . $file->baseName . '.' . $file->extension);
+	// 			} else {
+	// 				foreach($this->$attribute as $file) {
+	// 					$file->saveAs('assets/' . $file->baseName . '.' . $file->extension);
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+
+	// 	return true;
+	// }
 
 	/**
 	 * Returns the first error's attribute name
