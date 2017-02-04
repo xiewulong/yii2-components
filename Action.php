e295b2e6792fec8d0a35427e8e48dca293539315
xiewulong <xiewulong@vip.qq.com>
2017-02-04 16:30:14 +0800

    components

--- Action.php
+++ Action.php
@@ -1,10 +1,10 @@
 <?php
 /*!
- * yii2 - component - controller
+ * yii2 - component - action
  * xiewulong <xiewulong@vip.qq.com>
  * https://github.com/xiewulong/yii2-components
  * https://raw.githubusercontent.com/xiewulong/yii2-components/master/LICENSE
- * create: 2016/08/07
+ * create: 2017/01/24
  * update: 2017/01/24
  * since: 0.0.1
  */
@@ -14,16 +14,7 @@ namespace yii\components;
 use Yii;
 use yii\web\Response;
 
-class Controller extends \yii\web\Controller {
-
-	public function init() {
-		parent::init();
-
-		// set referrer from request
-		if($referrer = \Yii::$app->request->get('referrer')) {
-			\Yii::$app->user->returnUrl = $referrer;
-		}
-	}
+class Action extends \yii\base\Action {
 
 	/**
 	 * ajax response
